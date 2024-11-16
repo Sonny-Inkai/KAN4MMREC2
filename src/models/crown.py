eabfe384a7fe5ff6d7f5cab5f1849a1a47c5005e
sonnyinkai <sonnyinkai@gmail.com>
2024-11-16 19:26:56 +0700

    first commit

---
+++ src/models/crown.py
@@ -0,0 +1,153 @@
+# coding: utf-8
+# @email: enoche.chow@gmail.com
+
+import os
+import numpy as np
+import scipy.sparse as sp
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch_geometric.nn import GCNConv
+from torch_geometric.utils import remove_self_loops, add_self_loops
+
+from common.abstract_recommender import GeneralRecommender
+from common.loss import BPRLoss, EmbLoss, L2Loss
+from utils.utils import build_sim, compute_normalized_laplacian
+
+
+class CROWN(GeneralRecommender):
+    def __init__(self, config, dataset):
+        super(CROWN, self).__init__(config, dataset)
+
+        # Load dataset information
+        self.embedding_dim = config["embedding_size"]
+        self.feat_embed_dim = config["feat_embed_dim"]
+        self.n_layers = config["n_layers"]
+        self.dropout = config["dropout"]
+        self.knn_k = config["knn_k"]
+        self.lambda_coeff = config["lambda_coeff"]
+        self.reg_weight = config["reg_weight"]
+        self.reward_gamma = config["reward_gamma"]
+        self.contextual_factor = config["contextual_factor"]
+
+        self.n_users = self.dataset.n_users
+        self.n_items = self.dataset.n_items
+        self.n_nodes = self.n_users + self.n_items
+
+        # Load interaction matrix
+        self.interaction_matrix = dataset.inter_matrix(form="coo").astype(np.float32)
+        self.norm_adj = self.get_norm_adj_mat().to(self.device)
+        self.edge_indices, self.edge_values = self.get_edge_info()
+        self.edge_indices, self.edge_values = self.edge_indices.to(self.device), self.edge_values.to(self.device)
+
+        # Initialize embeddings
+        self.user_embedding = nn.Embedding(self.n_users, self.embedding_dim)
+        self.item_embedding = nn.Embedding(self.n_items, self.embedding_dim)
+        nn.init.xavier_uniform_(self.user_embedding.weight)
+        nn.init.xavier_uniform_(self.item_embedding.weight)
+
+        # Initialize Graph Convolution layers
+        self.gcn_layers = nn.ModuleList([GCNConv(self.embedding_dim, self.embedding_dim) for _ in range(self.n_layers)])
+
+        # Modality embeddings
+        if hasattr(self, 'v_feat') and self.v_feat is not None:
+            self.image_embedding = nn.Embedding.from_pretrained(self.v_feat, freeze=False)
+            self.image_trs = nn.Linear(self.v_feat.shape[1], self.feat_embed_dim)
+        if hasattr(self, 't_feat') and self.t_feat is not None:
+            self.text_embedding = nn.Embedding.from_pretrained(self.t_feat, freeze=False)
+            self.text_trs = nn.Linear(self.t_feat.shape[1], self.feat_embed_dim)
+
+        # Reinforcement Learning Parameters
+        self.q_network = nn.Linear(self.embedding_dim * 2, 1)
+        self.target_network = nn.Linear(self.embedding_dim * 2, 1)
+        nn.init.xavier_uniform_(self.q_network.weight)
+        nn.init.xavier_uniform_(self.target_network.weight)
+
+    def get_norm_adj_mat(self):
+        A = sp.dok_matrix((self.n_users + self.n_items, self.n_users + self.n_items), dtype=np.float32)
+        inter_M = self.interaction_matrix
+        inter_M_t = self.interaction_matrix.transpose()
+        data_dict = dict(zip(zip(inter_M.row, inter_M.col + self.n_users), [1] * inter_M.nnz))
+        data_dict.update(dict(zip(zip(inter_M_t.row + self.n_users, inter_M_t.col), [1] * inter_M_t.nnz)))
+        A._update(data_dict)
+        sumArr = (A > 0).sum(axis=1)
+        diag = np.array(sumArr.flatten())[0] + 1e-7
+        diag = np.power(diag, -0.5)
+        D = sp.diags(diag)
+        L = D * A * D
+        L = sp.coo_matrix(L)
+        row = L.row
+        col = L.col
+        i = torch.LongTensor(np.array([row, col]))
+        data = torch.FloatTensor(L.data)
+
+        return torch.sparse.FloatTensor(i, data, torch.Size((self.n_nodes, self.n_nodes)))
+
+    def forward(self, adj):
+        h = self.item_embedding.weight
+        for i in range(self.n_layers):
+            h = F.relu(self.gcn_layers[i](h, adj))
+        h = F.dropout(h, p=self.dropout, training=self.training)
+
+        ego_embeddings = torch.cat((self.user_embedding.weight, self.item_embedding.weight), dim=0)
+        all_embeddings = [ego_embeddings]
+        for i in range(self.n_layers):
+            side_embeddings = torch.sparse.mm(adj, ego_embeddings)
+            ego_embeddings = side_embeddings
+            all_embeddings += [ego_embeddings]
+        all_embeddings = torch.stack(all_embeddings, dim=1)
+        all_embeddings = all_embeddings.mean(dim=1, keepdim=False)
+        u_g_embeddings, i_g_embeddings = torch.split(all_embeddings, [self.n_users, self.n_items], dim=0)
+        return u_g_embeddings, i_g_embeddings + h
+
+    def calculate_loss(self, interaction):
+        users = interaction[0]
+        pos_items = interaction[1]
+        neg_items = interaction[2]
+
+        ua_embeddings, ia_embeddings = self.forward(self.norm_adj)
+
+        u_g_embeddings = ua_embeddings[users]
+        pos_i_g_embeddings = ia_embeddings[pos_items]
+        neg_i_g_embeddings = ia_embeddings[neg_items]
+
+        pos_scores = torch.sum(torch.mul(u_g_embeddings, pos_i_g_embeddings), dim=1)
+        neg_scores = torch.sum(torch.mul(u_g_embeddings, neg_i_g_embeddings), dim=1)
+
+        maxi = F.logsigmoid(pos_scores - neg_scores)
+        mf_loss = -torch.mean(maxi)
+
+        # Reinforcement learning-based reward adjustment
+        context_vec = torch.cat((u_g_embeddings, pos_i_g_embeddings), dim=1)
+        q_values = self.q_network(context_vec)
+        target_values = self.target_network(context_vec).detach()
+        rl_loss = F.mse_loss(q_values, target_values)
+
+        return mf_loss + self.lambda_coeff * rl_loss
+
+    def full_sort_predict(self, interaction):
+        user = interaction[0]
+
+        restore_user_e, restore_item_e = self.forward(self.norm_adj)
+        u_embeddings = restore_user_e[user]
+
+        scores = torch.matmul(u_embeddings, restore_item_e.transpose(0, 1))
+        return scores
+
+    def get_edge_info(self):
+        rows = torch.from_numpy(self.interaction_matrix.row)
+        cols = torch.from_numpy(self.interaction_matrix.col)
+        edges = torch.stack([rows, cols]).type(torch.LongTensor)
+        values = self._normalize_adj_m(edges, torch.Size((self.n_users, self.n_items)))
+        return edges, values
+
+    def _normalize_adj_m(self, indices, adj_size):
+        adj = torch.sparse.FloatTensor(indices, torch.ones_like(indices[0]), adj_size)
+        row_sum = 1e-7 + torch.sparse.sum(adj, -1).to_dense()
+        col_sum = 1e-7 + torch.sparse.sum(adj.t(), -1).to_dense()
+        r_inv_sqrt = torch.pow(row_sum, -0.5)
+        rows_inv_sqrt = r_inv_sqrt[indices[0]]
+        c_inv_sqrt = torch.pow(col_sum, -0.5)
+        cols_inv_sqrt = c_inv_sqrt[indices[1]]
+        values = rows_inv_sqrt * cols_inv_sqrt
+        return values
