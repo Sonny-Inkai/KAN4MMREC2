2cfb1230c3a05d5b97f8f3a7380451de10362546
sonnyinkai <sonnyinkai@gmail.com>
2024-11-16 19:33:40 +0700

    first commit

--- src/models/crown.py
+++ src/models/crown.py
@@ -69,7 +69,7 @@ class CROWN(GeneralRecommender):
         inter_M_t = self.interaction_matrix.transpose()
         data_dict = dict(zip(zip(inter_M.row, inter_M.col + self.n_users), [1] * inter_M.nnz))
         data_dict.update(dict(zip(zip(inter_M_t.row + self.n_users, inter_M_t.col), [1] * inter_M_t.nnz)))
-        A._update(data_dict)
+        A.update(data_dict)
         sumArr = (A > 0).sum(axis=1)
         diag = np.array(sumArr.flatten())[0] + 1e-7
         diag = np.power(diag, -0.5)
