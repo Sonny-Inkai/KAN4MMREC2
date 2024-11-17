2311366fa508d7f9685ef0d3b5624b62afe24d19
sonnyinkai <sonnyinkai@gmail.com>
2024-11-17 18:14:39 +0700

    first commit

--- src/models/amgan.py
+++ src/models/amgan.py
@@ -1,7 +1,7 @@
 # coding: utf-8
 # @email: enoche.chow@gmail.com
 r"""
-HCM-Rec: Hierarchical Cross-Modal Transformer for Multimedia Recommendations
+MHGT: Multimodal Hierarchical Graph Transformer for Multimedia Recommendations
 # Update: 17/11/2024
 """
 
@@ -10,6 +10,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 import numpy as np
 from torch.nn import Parameter
+from torch_geometric.nn import GCNConv
 from common.abstract_recommender import GeneralRecommender
 from common.loss import BPRLoss, EmbLoss
 
@@ -35,6 +36,17 @@ class HierarchicalAttention(nn.Module):
     def forward(self, query, key, value):
         attn_output, _ = self.attention_layer(query, key, value)
         return attn_output
+
+class GraphFeaturePropagation(nn.Module):
+    def __init__(self, embedding_dim):
+        super(GraphFeaturePropagation, self).__init__()
+        self.gcn1 = GCNConv(embedding_dim, embedding_dim)
+        self.gcn2 = GCNConv(embedding_dim, embedding_dim)
+
+    def forward(self, x, edge_index):
+        x = F.relu(self.gcn1(x, edge_index))
+        x = self.gcn2(x, edge_index)
+        return x
 
 class AMGAN(GeneralRecommender):
     def __init__(self, config, dataset):
@@ -67,7 +79,10 @@ class AMGAN(GeneralRecommender):
         # Hierarchical Attention Module
         self.hierarchical_attention = HierarchicalAttention(self.embedding_dim)
 
-    def forward(self, user_indices, item_indices):
+        # Graph Feature Propagation Module
+        self.graph_feature_propagation = GraphFeaturePropagation(self.embedding_dim)
+
+    def forward(self, user_indices, item_indices, edge_index):
         user_embed = self.user_embedding(user_indices)
         item_embed = self.item_embedding(item_indices)
 
@@ -88,16 +103,19 @@ class AMGAN(GeneralRecommender):
         # Hierarchical Attention
         combined_item_feat = self.hierarchical_attention(combined_item_feat.unsqueeze(0), combined_item_feat.unsqueeze(0), combined_item_feat.unsqueeze(0)).squeeze(0)
 
+        # Graph Feature Propagation
+        combined_item_feat = self.graph_feature_propagation(combined_item_feat, edge_index)
+
         return user_embed, combined_item_feat
 
-    def calculate_loss(self, interaction):
+    def calculate_loss(self, interaction, edge_index):
         user_indices = interaction[0]
         pos_item_indices = interaction[1]
         neg_item_indices = interaction[2]
 
         # Forward Pass
-        user_embed, pos_item_embed = self.forward(user_indices, pos_item_indices)
-        _, neg_item_embed = self.forward(user_indices, neg_item_indices)
+        user_embed, pos_item_embed = self.forward(user_indices, pos_item_indices, edge_index)
+        _, neg_item_embed = self.forward(user_indices, neg_item_indices, edge_index)
 
         # BPR Loss
         pos_scores = torch.sum(user_embed * pos_item_embed, dim=1)
@@ -113,7 +131,7 @@ class AMGAN(GeneralRecommender):
 
         return bpr_loss + self.contrastive_weight * contrastive_loss + reg_loss
 
-    def full_sort_predict(self, interaction):
+    def full_sort_predict(self, interaction, edge_index):
         user_indices = interaction[0]
         user_embed = self.user_embedding(user_indices)
         item_embed = self.item_embedding.weight
@@ -133,6 +151,9 @@ class AMGAN(GeneralRecommender):
 
         # Hierarchical Attention for all items
         combined_item_feat = self.hierarchical_attention(combined_item_feat.unsqueeze(0), combined_item_feat.unsqueeze(0), combined_item_feat.unsqueeze(0)).squeeze(0)
+
+        # Graph Feature Propagation for all items
+        combined_item_feat = self.graph_feature_propagation(combined_item_feat, edge_index)
 
         # Dot product between user and all items
         scores = torch.matmul(user_embed, combined_item_feat.t())
