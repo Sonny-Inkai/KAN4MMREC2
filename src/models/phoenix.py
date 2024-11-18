7b1b3d464c071b6d622e32586384c49dbe628d5d
sonnyinkai <sonnyinkai@gmail.com>
2024-11-18 11:59:52 +0700

    first commit

--- src/models/phoenix.py
+++ src/models/phoenix.py
@@ -1,63 +1,57 @@
-# coding: utf-8
-# MULTI-GRAIL: Multi-modal Graph Refined Adaptive Integration Learner
-# Aiming for SOTA performance in multimodal recommendation.
-
+import os
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-import numpy as np
+from torch_geometric.nn import MessagePassing
+from torch_geometric.utils import degree
 import scipy.sparse as sp
-from common.abstract_recommender import GeneralRecommender
-from common.loss import BPRLoss, EmbLoss
-from torch.nn.functional import cosine_similarity
+import numpy as np
 
-class PHOENIX(GeneralRecommender):
+# Proposed SOTA Model for Multimodal Recommendation
+class PHOENIX(nn.Module):
     def __init__(self, config, dataset):
-        super(PHOENIX, self).__init__(config, dataset)
-        
-        self.embedding_dim = config["embedding_size"]
-        self.feat_embed_dim = config["feat_embed_dim"]
-        self.n_layers = config["n_mm_layers"]
-        self.reg_weight = config["reg_weight"]
-        self.cl_weight = config["cl_weight"]
-        self.knn_k = config["knn_k"]
-        self.mm_image_weight = config["mm_image_weight"]
-        self.dropout = config["dropout"]
+        super(PHOENIX, self).__init__()
+        self.embedding_dim = config['embedding_size']
+        self.feat_embed_dim = config['feat_embed_dim']
+        self.knn_k = config['knn_k']
+        self.lambda_coeff = config['lambda_coeff']
+        self.cf_model = config['cf_model']
+        self.n_layers = config['n_layers']
+        self.reg_weight = config['reg_weight']
+        self.device = config['device']
 
+        # Load dataset information
+        self.n_users = dataset.n_users
+        self.n_items = dataset.n_items
         self.n_nodes = self.n_users + self.n_items
+        self.interaction_matrix = dataset.inter_matrix(form='coo').astype(np.float32)
+        self.norm_adj = self.get_norm_adj_mat(self.interaction_matrix).to(self.device)
 
-        # Adjacency matrix (interaction graph)
-        self.norm_adj = self.get_norm_adj_mat(dataset.inter_matrix(form="coo").astype(np.float32)).to(self.device)
-
-        # User and Item embeddings
+        # Embedding layers
         self.user_embedding = nn.Embedding(self.n_users, self.embedding_dim)
-        self.item_id_embedding = nn.Embedding(self.n_items, self.embedding_dim)
+        self.item_embedding = nn.Embedding(self.n_items, self.embedding_dim)
         nn.init.xavier_uniform_(self.user_embedding.weight)
-        nn.init.xavier_uniform_(self.item_id_embedding.weight)
+        nn.init.xavier_uniform_(self.item_embedding.weight)
 
-        # Visual and textual embeddings (if available)
-        if self.v_feat is not None:
-            self.image_embedding = nn.Embedding.from_pretrained(self.v_feat, freeze=False)
-            self.image_trs = nn.Linear(self.v_feat.shape[1], self.feat_embed_dim)
+        if dataset.v_feat is not None:
+            self.image_embedding = nn.Embedding.from_pretrained(dataset.v_feat, freeze=False)
+            self.image_trs = nn.Linear(dataset.v_feat.shape[1], self.feat_embed_dim)
             nn.init.xavier_normal_(self.image_trs.weight)
-        if self.t_feat is not None:
-            self.text_embedding = nn.Embedding.from_pretrained(self.t_feat, freeze=False)
-            self.text_trs = nn.Linear(self.t_feat.shape[1], self.feat_embed_dim)
+
+        if dataset.t_feat is not None:
+            self.text_embedding = nn.Embedding.from_pretrained(dataset.t_feat, freeze=False)
+            self.text_trs = nn.Linear(dataset.t_feat.shape[1], self.feat_embed_dim)
             nn.init.xavier_normal_(self.text_trs.weight)
 
-        # Predictor for contrastive learning
-        self.predictor = nn.Linear(self.embedding_dim, self.embedding_dim)
-        nn.init.xavier_normal_(self.predictor.weight)
-
-        # Regularization loss
-        self.reg_loss = EmbLoss()
-
-        # Multi-head attention weights for graph and modalities
-        self.modal_weights = nn.Parameter(torch.Tensor([0.5, 0.5]))  # Textual, Visual
-        self.softmax = nn.Softmax(dim=0)
+        # Graph learning components
+        self.graph_layers = nn.ModuleList([GraphLayer(self.embedding_dim) for _ in range(self.n_layers)])
+        self.cross_modal_correction = CrossModalCorrection(self.embedding_dim)
+        self.transformer_encoder = nn.TransformerEncoder(
+            nn.TransformerEncoderLayer(d_model=self.embedding_dim, nhead=8, dropout=config['dropout']),
+            num_layers=config['n_layers']
+        )
 
     def get_norm_adj_mat(self, interaction_matrix):
-        """Generates normalized adjacency matrix for user-item interactions."""
         A = sp.dok_matrix((self.n_users + self.n_items, self.n_users + self.n_items), dtype=np.float32)
         inter_M = interaction_matrix
         inter_M_t = interaction_matrix.transpose()
@@ -66,7 +60,7 @@ class PHOENIX(GeneralRecommender):
         for key, value in data_dict.items():
             A[key] = value
         sumArr = (A > 0).sum(axis=1)
-        diag = np.array(sumArr.flatten())[0] + 1e-7
+        diag = np.array(sumArr.flatten()).astype(np.float32) + 1e-7
         diag = np.power(diag, -0.5)
         D = sp.diags(diag)
         L = D * A * D
@@ -77,62 +71,63 @@ class PHOENIX(GeneralRecommender):
         data = torch.FloatTensor(L.data)
         return torch.sparse.FloatTensor(i, data, torch.Size((self.n_nodes, self.n_nodes)))
 
-    def forward(self):
-        """Generates embeddings using multi-modal and interaction graphs."""
-        # Propagate item embeddings through multi-modal graphs
-        h_item = self.item_id_embedding.weight
-        for _ in range(self.n_layers):
-            h_item = torch.sparse.mm(self.norm_adj, h_item)
+    def forward(self, users, items):
+        user_emb = self.user_embedding(users)
+        item_emb = self.item_embedding(items)
 
-        # Multi-modal embeddings
-        t_feat, v_feat = None, None
-        if self.t_feat is not None:
-            t_feat = self.text_trs(self.text_embedding.weight)
-        if self.v_feat is not None:
-            v_feat = self.image_trs(self.image_embedding.weight)
+        # Graph learning and propagation
+        ego_embeddings = torch.cat([self.user_embedding.weight, self.item_embedding.weight], dim=0)
+        for layer in self.graph_layers:
+            ego_embeddings = layer(self.norm_adj, ego_embeddings)
+        user_graph_emb, item_graph_emb = torch.split(ego_embeddings, [self.n_users, self.n_items], dim=0)
 
-        # Fuse multi-modal embeddings
-        if self.v_feat is not None and self.t_feat is not None:
-            weight = self.softmax(self.modal_weights)
-            h_item += weight[0] * t_feat + weight[1] * v_feat
-        elif self.t_feat is not None:
-            h_item += t_feat
-        elif self.v_feat is not None:
-            h_item += v_feat
+        # Cross-modal correction mechanism
+        user_corrected, item_corrected = self.cross_modal_correction(user_graph_emb, item_graph_emb)
 
-        # User embeddings via GCN propagation
-        ego_embeddings = torch.cat((self.user_embedding.weight, self.item_id_embedding.weight), dim=0)
-        all_embeddings = [ego_embeddings]
-        for _ in range(self.n_layers):
-            ego_embeddings = torch.sparse.mm(self.norm_adj, ego_embeddings)
-            all_embeddings.append(ego_embeddings)
-        all_embeddings = torch.stack(all_embeddings, dim=1).mean(dim=1)
-        u_g_embeddings, i_g_embeddings = torch.split(all_embeddings, [self.n_users, self.n_items], dim=0)
+        # Transformer encoder for final user-item representation
+        user_final = self.transformer_encoder(user_corrected.unsqueeze(0)).squeeze(0)
+        item_final = self.transformer_encoder(item_corrected.unsqueeze(0)).squeeze(0)
 
-        return u_g_embeddings, i_g_embeddings + h_item
+        user_out = user_final[users]
+        item_out = item_final[items]
 
-    def calculate_loss(self, interactions):
-        """Combines BPR and contrastive learning losses."""
-        u_online, i_online = self.forward()
-        users, pos_items, neg_items = interactions[0], interactions[1], interactions[2]
+        return torch.sum(user_out * item_out, dim=1)
 
-        # BPR Loss
-        u_embed, pos_embed, neg_embed = u_online[users], i_online[pos_items], i_online[neg_items]
-        pos_scores = torch.sum(u_embed * pos_embed, dim=1)
-        neg_scores = torch.sum(u_embed * neg_embed, dim=1)
-        bpr_loss = -torch.mean(torch.log2(torch.sigmoid(pos_scores - neg_scores)))
+    def calculate_loss(self, interaction):
+        users, pos_items, neg_items = interaction
+        pos_scores = self.forward(users, pos_items)
+        neg_scores = self.forward(users, neg_items)
+        loss = -torch.mean(F.logsigmoid(pos_scores - neg_scores))
+        reg_loss = self.reg_weight * (
+            self.user_embedding.weight.norm(2).pow(2) +
+            self.item_embedding.weight.norm(2).pow(2)
+        )
+        return loss + reg_loss
 
-        # Contrastive Learning Loss
-        with torch.no_grad():
-            u_target, i_target = u_online.clone().detach(), i_online.clone().detach()
-        u_online, i_online = self.predictor(u_online), self.predictor(i_online)
-        contrastive_loss = 1 - cosine_similarity(u_online[users], i_target[pos_items], dim=-1).mean()
+class GraphLayer(MessagePassing):
+    def __init__(self, in_channels):
+        super(GraphLayer, self).__init__(aggr='add')
+        self.linear = nn.Linear(in_channels, in_channels)
+        nn.init.xavier_uniform_(self.linear.weight)
 
-        return bpr_loss + self.cl_weight * contrastive_loss + self.reg_weight * self.reg_loss(u_online, i_online)
+    def forward(self, edge_index, x):
+        x = self.linear(x)
+        return self.propagate(edge_index, x=x)
 
-    def full_sort_predict(self, interactions):
-        """Predicts scores for all items for a given user."""
-        user = interactions[0]
-        u_online, i_online = self.forward()
-        scores = torch.matmul(u_online[user], i_online.transpose(0, 1))
-        return scores
+    def message(self, x_j, edge_index, size):
+        row, col = edge_index
+        deg = degree(row, size[0], dtype=x_j.dtype)
+        deg_inv_sqrt = deg.pow(-0.5)
+        norm = deg_inv_sqrt[row] * deg_inv_sqrt[col]
+        return norm.view(-1, 1) * x_j
+
+class CrossModalCorrection(nn.Module):
+    def __init__(self, embedding_dim):
+        super(CrossModalCorrection, self).__init__()
+        self.transform = nn.Linear(embedding_dim, embedding_dim)
+        nn.init.xavier_uniform_(self.transform.weight)
+
+    def forward(self, user_emb, item_emb):
+        corrected_user = self.transform(user_emb)
+        corrected_item = self.transform(item_emb)
+        return corrected_user, corrected_item
