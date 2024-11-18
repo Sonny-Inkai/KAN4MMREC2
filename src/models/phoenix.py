eb052e8daffc819f1e6db1baa2082a6276e2aafb
sonnyinkai <sonnyinkai@gmail.com>
2024-11-18 16:54:02 +0700

    first commit

--- src/models/phoenix.py
+++ src/models/phoenix.py
@@ -13,8 +13,9 @@ import torch.nn.functional as F
 
 from common.abstract_recommender import GeneralRecommender
 from torch_geometric.nn import GATConv
-from torch_sparse import SparseTensor
-from torch_geometric.utils import dropout_adj
+from torch_geometric.utils import add_self_loops, remove_self_loops, to_undirected
+from torch_geometric.data import Data
+from torch_geometric.utils import dense_to_sparse
 
 
 class PHOENIX(GeneralRecommender):
@@ -28,6 +29,10 @@ class PHOENIX(GeneralRecommender):
         self.num_heads = config['num_heads']
         self.dropout_rate = config['dropout_rate']
         self.alpha = config['alpha']  # LeakyReLU negative slope
+        self.reg_weight = config['reg_weight']
+        self.ssl_weight = config['ssl_weight']
+        self.ssl_temp = config['ssl_temp']
+        self.ssl_dropout = config['ssl_dropout']
 
         # Device configuration
         self.device = config['device']
@@ -58,10 +63,10 @@ class PHOENIX(GeneralRecommender):
             self.gat_layers.append(
                 GATConv(
                     in_channels=self.embedding_dim,
-                    out_channels=self.embedding_dim // self.num_heads,
+                    out_channels=self.embedding_dim,
                     heads=self.num_heads,
+                    concat=False,
                     dropout=self.dropout_rate,
-                    concat=True,
                     negative_slope=self.alpha,
                 )
             )
@@ -69,10 +74,6 @@ class PHOENIX(GeneralRecommender):
         # Modality attention fusion layer
         self.modality_attention = nn.Linear(self.embedding_dim * 2, self.embedding_dim)
         nn.init.xavier_uniform_(self.modality_attention.weight)
-
-        # Final prediction layer
-        self.predictor = nn.Linear(self.embedding_dim, self.embedding_dim)
-        nn.init.xavier_uniform_(self.predictor.weight)
 
         # Loss function
         self.bce_loss = nn.BCEWithLogitsLoss()
@@ -82,36 +83,44 @@ class PHOENIX(GeneralRecommender):
 
     def prepare_graph(self, dataset):
         # Build user-item interaction graph
-        interaction_matrix = dataset.inter_matrix(form='coo').astype(np.float32)
-        edge_index = np.vstack((interaction_matrix.row, interaction_matrix.col + self.n_users))
-        edge_index = torch.tensor(edge_index, dtype=torch.long).to(self.device)
+        interaction_matrix = dataset.inter_matrix(form='coo').astype(np.int64)
+        user_indices = torch.from_numpy(interaction_matrix.row).to(self.device)
+        item_indices = torch.from_numpy(interaction_matrix.col).to(self.device) + self.n_users
+        edge_index_ui = torch.stack([user_indices, item_indices], dim=0)
 
         # Build item-item similarity graph based on multimodal features
-        item_feat = []
+        item_feat_list = []
         if self.v_feat_embed is not None:
-            item_feat.append(self.v_feat_embed)
+            item_feat_list.append(self.v_feat_embed)
         if self.t_feat_embed is not None:
-            item_feat.append(self.t_feat_embed)
-        if item_feat:
-            item_features = torch.cat(item_feat, dim=1)
-            item_sim = F.cosine_similarity(
-                item_features.unsqueeze(1), item_features.unsqueeze(0), dim=-1
-            )
-            # Construct item-item edges
+            item_feat_list.append(self.t_feat_embed)
+
+        if item_feat_list:
+            item_features = torch.cat(item_feat_list, dim=1)
+            # Compute cosine similarity
+            item_norm = item_features / item_features.norm(dim=1, keepdim=True)
+            similarity_matrix = torch.mm(item_norm, item_norm.t())
+
+            # Build adjacency matrix
             k = 10  # Number of nearest neighbors
-            _, top_k_indices = torch.topk(item_sim, k=k, dim=-1)
-            item_indices = torch.arange(self.n_items).unsqueeze(1).expand(-1, k).flatten()
-            neighbor_indices = top_k_indices.flatten()
-            item_edge_index = torch.stack([item_indices, neighbor_indices]) + self.n_users
-            edge_index = torch.cat([edge_index, item_edge_index], dim=1)
+            top_k_values, top_k_indices = torch.topk(similarity_matrix, k=k, dim=-1)
+            item_indices_row = torch.arange(self.n_items).unsqueeze(1).expand(-1, k).flatten().to(self.device)
+            item_indices_col = top_k_indices.flatten().to(self.device)
+            edge_index_ii = torch.stack([item_indices_row + self.n_users, item_indices_col + self.n_users], dim=0)
         else:
-            item_features = None
+            edge_index_ii = torch.empty((2, 0), dtype=torch.long).to(self.device)
 
-        # Convert to SparseTensor for efficient processing
+        # Combine edge indices
+        edge_index = torch.cat([edge_index_ui, edge_index_ui.flip([0]), edge_index_ii], dim=1)
+        edge_index = to_undirected(edge_index)
+
+        # Remove self-loops
+        edge_index, _ = remove_self_loops(edge_index)
+        edge_index = add_self_loops(edge_index, num_nodes=self.n_users + self.n_items)[0]
+
         self.edge_index = edge_index
-        self.adj_t = SparseTensor(row=edge_index[0], col=edge_index[1], sparse_sizes=(self.n_users + self.n_items, self.n_users + self.n_items)).to(self.device)
 
-    def forward(self, users, pos_items, neg_items):
+    def forward(self):
         # Generate node embeddings
         x = torch.cat([self.user_embedding.weight, self.item_embedding.weight], dim=0)
         all_embeddings = [x]
@@ -137,19 +146,17 @@ class PHOENIX(GeneralRecommender):
         elif self.t_feat_embed is not None:
             item_embeddings = item_embeddings + self.t_feat_embed
 
-        # Get user and item embeddings for the batch
-        u_embeddings = user_embeddings[users]
-        pos_i_embeddings = item_embeddings[pos_items]
-        neg_i_embeddings = item_embeddings[neg_items]
-
-        return u_embeddings, pos_i_embeddings, neg_i_embeddings
+        return user_embeddings, item_embeddings
 
     def calculate_loss(self, interaction):
         users = interaction[0]
         pos_items = interaction[1]
         neg_items = interaction[2]
 
-        u_embeddings, pos_i_embeddings, neg_i_embeddings = self.forward(users, pos_items, neg_items)
+        user_embeddings, item_embeddings = self.forward()
+        u_embeddings = user_embeddings[users]
+        pos_i_embeddings = item_embeddings[pos_items]
+        neg_i_embeddings = item_embeddings[neg_items]
 
         # BPR loss
         pos_scores = torch.mul(u_embeddings, pos_i_embeddings).sum(dim=1)
@@ -162,79 +169,55 @@ class PHOENIX(GeneralRecommender):
             u_embeddings.norm(2).pow(2) +
             pos_i_embeddings.norm(2).pow(2) +
             neg_i_embeddings.norm(2).pow(2)
-        ) / 2
-
-        # Total loss
-        loss = bpr_loss + self.config['reg_weight'] * reg_loss
+        ) / 2 / u_embeddings.size(0)
 
         # Self-supervised contrastive loss
-        ssl_loss = self.calculate_ssl_loss(u_embeddings, pos_i_embeddings)
-        loss += self.config['ssl_weight'] * ssl_loss
+        ssl_loss = self.calculate_ssl_loss(users, pos_items)
+
+        # Total loss
+        loss = bpr_loss + self.reg_weight * reg_loss + self.ssl_weight * ssl_loss
 
         return loss
 
-    def calculate_ssl_loss(self, u_embeddings, pos_i_embeddings):
+    def calculate_ssl_loss(self, users, pos_items):
         # Generate two augmented views
+        user_embeddings, item_embeddings = self.forward()
+
         def augment_embeddings(embeddings):
-            noise = F.dropout(embeddings, p=self.config['ssl_dropout'])
+            noise = F.dropout(embeddings, p=self.ssl_dropout, training=True)
             return embeddings + noise
 
-        u_embeddings_aug1 = augment_embeddings(u_embeddings)
-        u_embeddings_aug2 = augment_embeddings(u_embeddings)
+        u_embeddings_aug1 = augment_embeddings(user_embeddings[users])
+        u_embeddings_aug2 = augment_embeddings(user_embeddings[users])
 
-        pos_i_embeddings_aug1 = augment_embeddings(pos_i_embeddings)
-        pos_i_embeddings_aug2 = augment_embeddings(pos_i_embeddings)
+        pos_i_embeddings_aug1 = augment_embeddings(item_embeddings[pos_items])
+        pos_i_embeddings_aug2 = augment_embeddings(item_embeddings[pos_items])
 
-        # Compute contrastive loss
-        batch_size = u_embeddings.size(0)
-        temperature = self.config['ssl_temp']
+        # Compute contrastive loss for users
+        ssl_loss_u = self.info_nce_loss(u_embeddings_aug1, u_embeddings_aug2)
 
-        z1 = F.normalize(u_embeddings_aug1, dim=1)
-        z2 = F.normalize(u_embeddings_aug2, dim=1)
-
-        pos_scores = torch.exp(torch.sum(z1 * z2, dim=1) / temperature)
-        total_scores = torch.exp(torch.matmul(z1, z2.t()) / temperature).sum(dim=1)
-
-        ssl_loss_u = -torch.log(pos_scores / total_scores).mean()
-
-        z1 = F.normalize(pos_i_embeddings_aug1, dim=1)
-        z2 = F.normalize(pos_i_embeddings_aug2, dim=1)
-
-        pos_scores = torch.exp(torch.sum(z1 * z2, dim=1) / temperature)
-        total_scores = torch.exp(torch.matmul(z1, z2.t()) / temperature).sum(dim=1)
-
-        ssl_loss_i = -torch.log(pos_scores / total_scores).mean()
+        # Compute contrastive loss for items
+        ssl_loss_i = self.info_nce_loss(pos_i_embeddings_aug1, pos_i_embeddings_aug2)
 
         return ssl_loss_u + ssl_loss_i
+
+    def info_nce_loss(self, z1, z2):
+        temperature = self.ssl_temp
+        batch_size = z1.size(0)
+
+        z1 = F.normalize(z1, dim=1)
+        z2 = F.normalize(z2, dim=1)
+
+        pos_scores = torch.exp(torch.sum(z1 * z2, dim=1) / temperature)
+        total_scores = torch.exp(torch.mm(z1, z2.t()) / temperature).sum(dim=1)
+
+        loss = -torch.log(pos_scores / total_scores).mean()
+        return loss
 
     def full_sort_predict(self, interaction):
         users = interaction[0]
 
-        # Generate node embeddings
-        x = torch.cat([self.user_embedding.weight, self.item_embedding.weight], dim=0)
-        all_embeddings = [x]
-
-        for gat_layer in self.gat_layers:
-            x = gat_layer(x, self.edge_index)
-            x = F.elu(x)
-            x = F.dropout(x, p=self.dropout_rate, training=self.training)
-            all_embeddings.append(x)
-
-        x = torch.mean(torch.stack(all_embeddings, dim=1), dim=1)
-        user_embeddings, item_embeddings = x[:self.n_users], x[self.n_users:]
-
-        # Modality-specific item embeddings
-        if self.v_feat_embed is not None and self.t_feat_embed is not None:
-            modality_embeddings = torch.cat([self.v_feat_embed, self.t_feat_embed], dim=1)
-            modality_embeddings = self.modality_attention(modality_embeddings)
-            item_embeddings = item_embeddings + modality_embeddings
-
-        elif self.v_feat_embed is not None:
-            item_embeddings = item_embeddings + self.v_feat_embed
-
-        elif self.t_feat_embed is not None:
-            item_embeddings = item_embeddings + self.t_feat_embed
-
+        user_embeddings, item_embeddings = self.forward()
         u_embeddings = user_embeddings[users]
 
         # Compute scores
