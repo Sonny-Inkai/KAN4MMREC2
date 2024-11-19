6ba31674926fee31c6fb27bc26a833da0e0f7c69
sonnyinkai <sonnyinkai@gmail.com>
2024-11-19 10:34:52 +0700

    first commit

--- src/models/phoenix.py
+++ src/models/phoenix.py
@@ -80,7 +80,7 @@ class PHOENIX(GeneralRecommender):
         inter_M_t = self.interaction_matrix.transpose()
         data_dict = dict(zip(zip(inter_M.row, inter_M.col + self.n_users), [1] * inter_M.nnz))
         data_dict.update(dict(zip(zip(inter_M_t.row + self.n_users, inter_M_t.col), [1] * inter_M_t.nnz)))
-        for key, value in data_dict.items:
+        for key, value in data_dict.items():
             A[key] = value
         sumArr = (A > 0).sum(axis=1)
         diag = np.array(sumArr.flatten())[0] + 1e-7
