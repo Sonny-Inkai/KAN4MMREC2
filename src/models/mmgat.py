dae0a1e00f08fabbd9f5254235ad16f41b550041
sonnyinkai <sonnyinkai@gmail.com>
2024-12-17 15:13:24 +0700

    init

--- src/models/mmgat.py
+++ src/models/mmgat.py
@@ -29,7 +29,6 @@ class MMGAT(GeneralRecommender):
     def __init__(self, config, dataset):
         super(MMGAT, self).__init__(config, dataset)
         
-        # Hyperparameters
         self.embedding_dim = 64 
         self.feat_embed_dim = 64
         self.num_heads = 4
@@ -40,36 +39,29 @@ class MMGAT(GeneralRecommender):
         self.lambda_coeff = 0.5
         self.reg_weight = 1e-4
         
-        # Number of nodes
         self.n_nodes = self.n_users + self.n_items
-        
-        # Load interaction matrix
         self.interaction_matrix = dataset.inter_matrix(form='coo').astype(np.float32)
-        self.norm_adj = self.get_norm_adj_mat().to(self.device)
         
-        # Embeddings
-        self.user_embedding = nn.Embedding(self.n_users, self.embedding_dim)
-        self.item_id_embedding = nn.Embedding(self.n_items, self.embedding_dim)
+        self.user_embedding = nn.Embedding(self.n_users, self.embedding_dim).to(self.device)
+        self.item_id_embedding = nn.Embedding(self.n_items, self.embedding_dim).to(self.device)
         nn.init.xavier_uniform_(self.user_embedding.weight)
         nn.init.xavier_uniform_(self.item_id_embedding.weight)
         
-        # Modal-specific components
         if self.v_feat is not None:
-            self.image_embedding = nn.Embedding.from_pretrained(self.v_feat, freeze=False)
-            self.image_trs = nn.Linear(self.v_feat.shape[1], self.feat_embed_dim)
-            self.image_attn = FlashAttention(self.feat_embed_dim)
+            self.image_embedding = nn.Embedding.from_pretrained(self.v_feat).to(self.device)
+            self.image_trs = nn.Linear(self.v_feat.shape[1], self.feat_embed_dim).to(self.device)
+            self.image_attn = FlashAttention(self.feat_embed_dim).to(self.device)
             
         if self.t_feat is not None:
-            self.text_embedding = nn.Embedding.from_pretrained(self.t_feat, freeze=False)
-            self.text_trs = nn.Linear(self.t_feat.shape[1], self.feat_embed_dim)
-            self.text_attn = FlashAttention(self.feat_embed_dim)
+            self.text_embedding = nn.Embedding.from_pretrained(self.t_feat).to(self.device)
+            self.text_trs = nn.Linear(self.t_feat.shape[1], self.feat_embed_dim).to(self.device)
+            self.text_attn = FlashAttention(self.feat_embed_dim).to(self.device)
             
-        # Multi-modal fusion
-        self.modal_fusion = nn.Linear(self.feat_embed_dim * 2, self.feat_embed_dim)
-        self.modal_weight = nn.Parameter(torch.Tensor([0.5, 0.5]))
+        self.modal_fusion = nn.Linear(self.feat_embed_dim * 2, self.feat_embed_dim).to(self.device)
+        self.modal_weight = nn.Parameter(torch.Tensor([0.5, 0.5])).to(self.device)
         self.softmax = nn.Softmax(dim=0)
         
-        # Graph components
+        self.norm_adj = self.get_norm_adj_mat().to(self.device)
         self.mm_adj = None
         self.build_multimodal_graph()
 
@@ -92,7 +84,7 @@ class MMGAT(GeneralRecommender):
         indices = torch.LongTensor(np.array([L.row, L.col]))
         values = torch.FloatTensor(L.data)
         
-        return torch.sparse.FloatTensor(indices, values, torch.Size((self.n_nodes, self.n_nodes)))
+        return torch.sparse_coo_tensor(indices, values, (self.n_nodes, self.n_nodes))
 
     def build_multimodal_graph(self):
         if self.v_feat is not None:
@@ -100,27 +92,29 @@ class MMGAT(GeneralRecommender):
             image_norm = F.normalize(image_feats, p=2, dim=1)
             image_sim = torch.mm(image_norm, image_norm.t())
             image_adj = self.build_knn_graph(image_sim)
-            self.mm_adj = image_adj
+            self.mm_adj = image_adj.to(self.device)
             
         if self.t_feat is not None:
             text_feats = self.text_trs(self.text_embedding.weight)
             text_norm = F.normalize(text_feats, p=2, dim=1)
             text_sim = torch.mm(text_norm, text_norm.t())
             text_adj = self.build_knn_graph(text_sim)
-            self.mm_adj = text_adj if self.mm_adj is None else self.mm_adj + text_adj
+            if self.mm_adj is None:
+                self.mm_adj = text_adj.to(self.device)
+            else:
+                self.mm_adj = (self.mm_adj + text_adj.to(self.device))
             
         if self.mm_adj is not None:
             self.mm_adj = F.normalize(self.mm_adj, p=1, dim=1)
 
     def build_knn_graph(self, sim_matrix):
         values, indices = sim_matrix.topk(k=self.knn_k, dim=1)
-        row_indices = torch.arange(sim_matrix.size(0)).view(-1, 1).expand_as(indices)
+        row_indices = torch.arange(sim_matrix.size(0), device=sim_matrix.device).view(-1, 1).expand_as(indices)
         adj = torch.zeros_like(sim_matrix)
         adj[row_indices.flatten(), indices.flatten()] = values.flatten()
         return adj
 
     def forward(self):
-        # Multi-modal feature processing
         mm_embeddings = None
         
         if self.v_feat is not None:
@@ -131,10 +125,11 @@ class MMGAT(GeneralRecommender):
         if self.t_feat is not None:
             text_feats = self.text_trs(self.text_embedding.weight)
             text_feats = self.text_attn(text_feats.unsqueeze(0)).squeeze(0)
-            mm_embeddings = text_feats if mm_embeddings is None else \
-                          self.modal_fusion(torch.cat([mm_embeddings, text_feats], dim=1))
+            if mm_embeddings is None:
+                mm_embeddings = text_feats
+            else:
+                mm_embeddings = self.modal_fusion(torch.cat([mm_embeddings, text_feats], dim=1))
 
-        # Graph propagation
         ego_embeddings = torch.cat((self.user_embedding.weight, self.item_id_embedding.weight), dim=0)
         all_embeddings = [ego_embeddings]
         
@@ -155,7 +150,39 @@ class MMGAT(GeneralRecommender):
         
         u_g_embeddings, i_g_embeddings = torch.split(all_embeddings, [self.n_users, self.n_items], dim=0)
         return u_g_embeddings, i_g_embeddings
+
+    def calculate_loss(self, interaction):
+        users = interaction[0].to(self.device)
+        pos_items = interaction[1].to(self.device)
+        neg_items = interaction[2].to(self.device)
+
+        u_embeddings, i_embeddings = self.forward()
         
+        user_embeddings = u_embeddings[users]
+        pos_embeddings = i_embeddings[pos_items]
+        neg_embeddings = i_embeddings[neg_items]
+
+        mf_loss = self.mirror_gradient_loss(users, pos_items, neg_items)
+        
+        modal_loss = 0.0
+        if self.v_feat is not None and self.t_feat is not None:
+            image_feats = self.image_trs(self.image_embedding.weight)
+            text_feats = self.text_trs(self.text_embedding.weight)
+            
+            pos_image_feats = image_feats[pos_items]
+            pos_text_feats = text_feats[pos_items]
+            
+            modal_sim = cosine_similarity(pos_image_feats, pos_text_feats, dim=1)
+            modal_loss = -torch.mean(torch.log(torch.sigmoid(modal_sim)))
+
+        reg_loss = self.reg_weight * (
+            torch.norm(user_embeddings) +
+            torch.norm(pos_embeddings) +
+            torch.norm(neg_embeddings)
+        )
+
+        return mf_loss + 0.1 * modal_loss + reg_loss
+
     def mirror_gradient_loss(self, users, pos_items, neg_items, alpha=0.2):
         u_embeddings = self.user_embedding(users)
         pos_embeddings = self.item_id_embedding(pos_items)
@@ -166,7 +193,6 @@ class MMGAT(GeneralRecommender):
         
         base_loss = -torch.mean(torch.log(torch.sigmoid(pos_scores - neg_scores)))
         
-        # Mirror gradient component
         with torch.no_grad():
             u_grad = torch.autograd.grad(base_loss, u_embeddings, retain_graph=True)[0]
             mirror_direction = -alpha * u_grad
@@ -179,43 +205,8 @@ class MMGAT(GeneralRecommender):
         
         return base_loss + mirror_loss
 
-    def calculate_loss(self, interaction):
-        users = interaction[0]
-        pos_items = interaction[1]
-        neg_items = interaction[2]
-
-        u_embeddings, i_embeddings = self.forward()
-        
-        user_embeddings = u_embeddings[users]
-        pos_embeddings = i_embeddings[pos_items]
-        neg_embeddings = i_embeddings[neg_items]
-
-        # BPR loss with mirror gradient
-        mf_loss = self.mirror_gradient_loss(users, pos_items, neg_items)
-        
-        # Modal contrastive loss
-        modal_loss = 0.0
-        if self.v_feat is not None and self.t_feat is not None:
-            image_feats = self.image_trs(self.image_embedding.weight)
-            text_feats = self.text_trs(self.text_embedding.weight)
-            
-            pos_image_feats = image_feats[pos_items]
-            pos_text_feats = text_feats[pos_items]
-            
-            modal_sim = cosine_similarity(pos_image_feats, pos_text_feats, dim=1)
-            modal_loss = -torch.mean(torch.log(torch.sigmoid(modal_sim)))
-
-        # Regularization
-        reg_loss = self.reg_weight * (
-            torch.norm(user_embeddings) +
-            torch.norm(pos_embeddings) +
-            torch.norm(neg_embeddings)
-        )
-
-        return mf_loss + 0.1 * modal_loss + reg_loss
-
     def full_sort_predict(self, interaction):
-        user = interaction[0]
+        user = interaction[0].to(self.device)
         
         u_embeddings, i_embeddings = self.forward()
         u_embeddings = u_embeddings[user]
